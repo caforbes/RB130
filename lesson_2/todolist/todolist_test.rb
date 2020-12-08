4f9239bde2acc4f681c3668cdabeda9ec8286fdc
Clarissa Forbes <for.c.forbes@gmail.com>
2020-12-08 17:05:14 +0000

    L2: additional todolist tests, 100% coverage

--- lesson_2/todolist/todolist_test.rb
+++ lesson_2/todolist/todolist_test.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest/autorun'
 require "minitest/reporters"
 Minitest::Reporters.use!
@@ -12,14 +15,13 @@ class TodoListTest < MiniTest::Test
     @todo3 = Todo.new("Go to gym")
     @todos = [@todo1, @todo2, @todo3]
 
-    @list = TodoList.new("Today's Todos")
+    @list = TodoList.new("Today's Test Todos")
     @list.add(@todo1)
     @list.add(@todo2)
     @list.add(@todo3)
   end
 
   # Your tests go here. Remember they must start with "test_"
-
   def test_to_a
     assert_equal @todos, @list.to_a
   end
@@ -86,4 +88,156 @@ class TodoListTest < MiniTest::Test
     assert_raises(IndexError) { @list.item_at(100) }
   end
 
+  def test_mark_done_at
+    @list.mark_done_at(0)
+
+    assert_equal true, @todo1.done?
+    assert_equal false, @todo2.done?
+    assert_equal false, @todo3.done?
+
+    assert_raises(IndexError) { @list.mark_done_at(100) }
+  end
+
+  def test_mark_undone_at
+    @todos.each { |todo| todo.done! }
+    @list.mark_undone_at(1)
+
+    assert_equal true, @todo1.done?
+    assert_equal false, @todo2.done?
+    assert_equal true, @todo3.done?
+
+    assert_raises(IndexError) { @list.mark_undone_at(100) }
+  end
+
+  def test_done_bang
+    @list.done!
+
+    assert_equal true, @todo1.done?
+    assert_equal true, @todo2.done?
+    assert_equal true, @todo3.done?
+    assert_equal true, @list.done?
+  end
+
+  def test_remove_at
+    assert_equal @todo2, @list.remove_at(1)
+    assert_equal [@todo1, @todo3], @list.to_a
+
+    assert_raises(IndexError) { @list.remove_at(100) }
+  end
+
+  def test_to_s
+    output = <<~OUTPUT.chomp
+              ---- Today's Test Todos ----
+              [ ] Buy milk
+              [ ] Clean room
+              [ ] Go to gym
+              OUTPUT
+
+    assert_equal output, @list.to_s
+  end
+
+  def test_to_s_some_done
+    output = <<~OUTPUT.chomp
+              ---- Today's Test Todos ----
+              [X] Buy milk
+              [ ] Clean room
+              [ ] Go to gym
+              OUTPUT
+
+    @todo1.done!
+    assert_equal output, @list.to_s
+  end
+
+  def test_to_s_all_done
+    output = <<~OUTPUT.chomp
+              ---- Today's Test Todos ----
+              [X] Buy milk
+              [X] Clean room
+              [X] Go to gym
+              OUTPUT
+
+    @list.done!
+    assert_equal output, @list.to_s
+  end
+
+  def test_each
+    iterations = []
+    @list.each { |todo| iterations << todo }
+
+    assert_equal @todos, iterations
+  end
+
+  def test_each_returns_orig
+    assert_equal @list, @list.each {}
+  end
+
+  def test_select
+    assert_equal [], @list.select { |_| false }.to_a
+    assert_equal @list.to_a, @list.select { |_| true }.to_a
+    assert_equal @list.title, @list.select { |_| true }.title
+
+    @todo1.done!
+    assert_equal [@todo1], @list.select { |todo| todo.done? }.to_a
+  end
+
+  def test_select_new_obj
+    refute_same @list, @list.select { |_| true }
+  end
+
+  def test_all_done
+    assert_equal [], @list.all_done.to_a
+    @todo1.done!
+    assert_equal [@todo1], @list.all_done.to_a
+    @todo2.done!
+    @todo3.done!
+    assert_equal @todos, @list.all_done.to_a
+  end
+
+  def test_all_not_done
+    assert_equal @todos, @list.all_not_done.to_a
+    @todo1.done!
+    @todo2.done!
+    assert_equal [@todo3], @list.all_not_done.to_a
+    @todo3.done!
+    assert_equal [], @list.all_not_done.to_a
+  end
+
+  def test_find_by_title
+    assert_equal @todo1, @list.find_by_title("Buy milk")
+    assert_nil @list.find_by_title("Eat candy")
+  end
+
+  def test_mark_done
+    assert_equal @todo1, @list.mark_done("Buy milk")
+    assert_equal true, @todo1.done?
+    assert_equal false, @todo2.done?
+    assert_equal false, @todo3.done?
+  end
+
+  def test_mark_undone
+    @list.done!
+    assert_equal @todo1, @list.mark_undone("Buy milk")
+    assert_equal false, @todo1.done?
+    assert_equal true, @todo2.done?
+    assert_equal true, @todo3.done?
+  end
+
+  def test_mark_done_bad_input
+    assert_nil @list.mark_done("Eat candy")
+    assert_equal @todos, @list.to_a
+  end
+
+  def test_mark_all_done
+    assert_equal @list, @list.mark_all_done
+    assert_equal true, @list.done?
+  end
+
+  def test_mark_all_undone
+    @list.done!
+    assert_equal @list, @list.mark_all_undone
+    assert_equal false, @list.done?
+    assert_equal false, @todo1.done?
+    assert_equal false, @todo2.done?
+    assert_equal false, @todo3.done?
+  end
 end
