ef285645ec2d7fecd00ab014d5b13adcf8096e19
Clarissa Forbes <for.c.forbes@gmail.com>
2020-12-10 17:57:45 +0000

    130 problems easy1: 6-10

---
+++ ex/easy_1/09_one.rb
@@ -0,0 +1,19 @@
+def one?(collection)
+  true_count, target = 0, 1
+
+  if block_given?
+    collection.each do |item|
+      true_count += 1 if yield(item)
+      return false if true_count > target
+    end
+  end
+  true_count == target
+end
+
+p one?([1, 3, 5, 6]) { |value| value.even? }    # -> true
+p one?([1, 3, 5, 7]) { |value| value.odd? }     # -> false
+p one?([2, 4, 6, 8]) { |value| value.even? }    # -> false
+p one?([1, 3, 5, 7]) { |value| value % 5 == 0 } # -> true
+p one?([1, 3, 5, 7]) { |value| true }           # -> false
+p one?([1, 3, 5, 7]) { |value| false }          # -> false
+p one?([]) { |value| true }                     # -> false
