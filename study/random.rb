9951b13a67de9b9b326e805b75794da73b4b4199
Clarissa Forbes <for.c.forbes@gmail.com>
2021-01-11 17:03:47 +0000

    study guide snippets

--- study/random.rb
+++ study/random.rb
@@ -1,8 +1,11 @@
-def use_block
-  block_return = yield
-  puts block_return
+def random_method(&block_arg)
+  puts 'do stuff'
+  run_here(block_arg)
+  puts 'do more stuff'
 end
 
-outer = 'hello'
+def run_here(some_proc)
+  some_proc.call
+end
 
-use_block { outer }
+random_method { puts 1 + 1 }
