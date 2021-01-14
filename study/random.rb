54b08915d85f97bba443434a54059310dc5093c4
Clarissa Forbes <for.c.forbes@gmail.com>
2021-01-14 17:23:31 +0000

    study snippets

--- study/random.rb
+++ study/random.rb
@@ -1,9 +1,1 @@
-def pass_nums(function)
-  function.call(1, 2, 3, 4)
-end
-
-lambda_4 = lambda { |a,b,c,d| puts "#{a} + #{b} + #{c} + #{d}" }
-lambda_1 = lambda { |a| puts "#{a} + b" }
-
-pass_nums(lambda_4) # => 1 + 2 + 3 + 4
-pass_nums(lambda_1) # ArgumentError
+p ['a', 'b', 'c'].map(&:upcase) # => ["A", "B", "C"]
