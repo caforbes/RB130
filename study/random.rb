587e1c71e2a61cf64f82b3b0f68a9167784326ef
Clarissa Forbes <for.c.forbes@gmail.com>
2021-01-15 00:18:56 +0000

    study guide snippets

--- study/random.rb
+++ study/random.rb
@@ -1,1 +1,8 @@
-p ['a', 'b', 'c'].map(&:upcase) # => ["A", "B", "C"]
+def sum_check(x, y, sum)
+  (x + y) == sum
+end
+
+array = [1, 2, 3]
+
+p sum_check(*array) # => true
+p sum_check(array) # ArgumentError: 1 arg instead of 3
